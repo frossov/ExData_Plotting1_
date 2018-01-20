af4f87e92b049c0cb933afa0731d66590a8ce064
frossov <33908399+frossov@users.noreply.github.com>
2018-01-20 18:03:24 +0200

    Update full_code.r

--- full_code.r
+++ full_code.r
@@ -33,7 +33,7 @@ with(household.data, {
 legend("topright", col=c("black", "red", "blue"), lwd=c(1,1,1), 
        c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 dev.copy(png, file="plot3.png", height=480, width=480)
-#dev.off()
+dev.off()
 
 ##plot4
 par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
